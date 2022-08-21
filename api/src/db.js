b153c00193faec5c3a6b4cf4d0b59351418a6e0d
Chelolb <marcelolitwin@gmail.com>
2022-08-21 16:15:57 -0300

    newcss-deploy1

--- api/src/db.js
+++ api/src/db.js
@@ -4,12 +4,26 @@ const fs = require('fs');
 const path = require('path');
 const { throws } = require('assert');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_URI
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
+//  Conexion con DB PostgreSQL alojada en PC local
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+
+//  Conexion con DB PostgreSQL alojada en Heroku
+const sequelize = new Sequelize(`${DB_URI}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+
+  dialectOptions: {               // requerido para conectar a Heroku DB Postgresql
+    ssl:{                         //
+      require:true,               //
+      rejectUnauthorized: false   //
+    }                             //
+  }                               //
 });
 const basename = path.basename(__filename);
 
